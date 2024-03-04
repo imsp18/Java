792e9f90fa98a9c3281b4508213129ed4a89b1ba
imsp18 <shivangparmar537@gmail.com>
2024-03-04 23:29:03 +0530

    added Variables

---
+++ Basics/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args){
+        int age; // Declaring a variable
+        age = 20; // Assigning a value to the variable
+        System.out.println("I am " + age + " years old."); // Printing the value of the variable
+    }
+}
+
+// Output: I am 20 years old.
