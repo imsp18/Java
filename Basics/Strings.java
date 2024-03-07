483c46cf0f91cf8135f15c59d1146ced8186a6f1
imsp18 <shivangparmar537@gmail.com>
2024-03-07 15:30:28 +0530

    added Strings

---
+++ Basics/Strings.java
@@ -0,0 +1,7 @@
+public class Strings {
+    public static void main(String[] args){
+        String greeting = "Hello";
+        String name = "World";
+        System.out.println(greeting + " " + name); // concatenation
+    }
+}
