d96482518fb42a8fd590c1cad0aa8715ada14246
imsp18 <shivangparmar537@gmail.com>
2024-03-12 21:50:29 +0530

    added Short If...Else

---
+++ Basics/ShortIfElse.java
@@ -0,0 +1,7 @@
+public class ShortIfElse {
+    public static void main(String[] args) {
+        int age = 18;
+        String message = (age <= 18) ? "You are a minor" : "You are an adult";
+        System.out.println(message);
+    }
+}
