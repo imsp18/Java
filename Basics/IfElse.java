a23b91ba53da1e95dbca0c4da9c49a28406f3fce
imsp18 <shivangparmar537@gmail.com>
2024-03-12 21:47:42 +0530

    updated: user can take the input and if...else condition is applied

--- Basics/IfElse.java
+++ Basics/IfElse.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class IfElse {
     public static void main(String[] args) {
-        int time = 18;
-        if (time < 10){
-           System.out.println("Goof morning.");
-        } 
-        else{
-            System.out.println("Good evening.");
+        Scanner scanner = new Scanner(System.in); //get input from the user
+
+        System.out.print("Enter your age:"); //ask the user to enter their age
+
+        int age = scanner.nextInt(); //read the input from the user
+
+        if (age < 18){
+            System.out.println("You are a minor");
         }
-      }
+        else if(age >= 18 && age < 60){
+            System.out.println("You are an adult");
+        }
+        else{
+            System.out.println("You are a senior citizen");
+        }
+
+        scanner.close(); //close the scanner
+
+    }
 }
