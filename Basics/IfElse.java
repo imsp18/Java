d3f88670cedc9ae78293b1d29349dc36bc5b590f
imsp18 <shivangparmar537@gmail.com>
2024-03-12 21:54:34 +0530

    Updated: added comments

--- Basics/IfElse.java
+++ Basics/IfElse.java
@@ -10,13 +10,13 @@ public class IfElse {
 
         if (age < 18){
             System.out.println("You are a minor");
-        }
+        } //if the age is less than 18, print "You are a minor"
         else if(age >= 18 && age < 60){
             System.out.println("You are an adult");
-        }
+        } //if the age is greater than or equal to 18 and less than 60, print "You are an adult"
         else{
             System.out.println("You are a senior citizen");
-        }
+        } //if the age is greater than or equal to 60, print "You are a senior citizen"
 
         scanner.close(); //close the scanner
 
