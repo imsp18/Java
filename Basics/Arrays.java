82e68d60fff4baee6f04c43c5ed85fe9ed2819cb
imsp18 <shivangparmar537@gmail.com>
2024-03-14 22:04:47 +0530

    Added Arrays

---
+++ Basics/Arrays.java
@@ -0,0 +1,12 @@
+
+public class Arrays {
+    public static void main(String[] args){
+
+        int[] numbers = {1,2,3,4,5,6}; // Array of integers
+
+        for(int i=0;i<numbers.length;i++){
+            
+            System.out.println(numbers[i]);
+        } // Output: 1 2 3 4 5 6
+    } 
+}
