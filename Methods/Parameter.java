0cc307417aff098ff0dc4f7caa0eefc286331432
imsp18 <shivangparmar537@gmail.com>
2024-03-15 11:19:56 +0530

    Added Parameter for Method

---
+++ Methods/Parameter.java
@@ -0,0 +1,11 @@
+public class Parameter {
+    static void myMethod( String fname){
+        System.out.println(fname + " is funny");
+    }
+
+    public static void main(String[] args){
+        myMethod("Liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+}
