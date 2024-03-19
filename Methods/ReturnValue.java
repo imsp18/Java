b5c32ccb0489a10f0115136c8ec97dff6abf8513
imsp18 <shivangparmar537@gmail.com>
2024-03-19 12:10:55 +0530

    added Return Value

---
+++ Methods/ReturnValue.java
@@ -0,0 +1,9 @@
+public class ReturnValue {
+    static int sum(int x){
+        return x + 1; // return statement
+    }
+
+    public static void main(String[] args){
+        System.out.print(sum(10));   // 11
+    }
+}
