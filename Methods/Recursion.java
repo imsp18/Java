851d55a621b0ad605fd0a05697d53c1f147616bf
imsp18 <shivangparmar537@gmail.com>
2024-03-21 21:48:06 +0530

    updated Recursion code

--- Methods/Recursion.java
+++ Methods/Recursion.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class Recursion {
     public static void main(String[] args){ 
-        int result = sum(10); 
-        System.out.println(result);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int result = input.nextInt();
+        System.out.println(sum(result));
+
+        input.close();
     }
     public static int sum(int k){
         if(k>0){
