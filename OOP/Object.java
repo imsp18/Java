349de8653ed7fedbc73137107d7ddab98c80ba6d
imsp18 <shivangparmar537@gmail.com>
2024-03-23 23:12:10 +0530

    Added Objects

---
+++ OOP/Object.java
@@ -0,0 +1,11 @@
+public class Object{
+    int x = 5;
+
+    public static void main(String[] args){
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+
+        System.out.println(obj1.x);
+        System.out.println(obj2.x);
+    }
+}
